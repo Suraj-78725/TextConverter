fb40755e888303e8aad7fb7c927c0af1d872aaf7
Suraj-78725 <surajbansode2004@gmail.com>
2024-06-30 22:09:06 +0530

    dark mode ia applied on all pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 
+import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
-// import Textform from './components/Textform';
-
+import Textform from './components/Textform';
 function App() {
+  const [mode,setMode]=useState("light");
+  const toggleMode=()=>
+    {
+      if(mode==="light")
+        {
+          setMode("dark");
+          document.body.style.backgroundColor="black";
+          // document.body.style.textColor="white";
+        }
+        else
+        {
+          setMode("light")
+          document.body.style.backgroundColor="white";
+        }
+    }
   return (
     <>
- <Navbar title="textutils" about="About textutils"/>
+ <Navbar title="textutils" about="About textutils" mode={mode} toggleMode={toggleMode} />
  <div className="container">
- {/* <Textform heading="Enter the text to analyze"/> */}
- <About/>
+ <Textform heading="Enter the text to analyze" mode={mode}/>
+ {/* <About/> */}
  </div>
      </>
   );
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,3 +1,4 @@
+import { getElementError } from '@testing-library/react';
 import React,{useState} from 'react'
 // import PropTypes from 'prop-types'
 export default function Textform(props) {
@@ -17,26 +18,39 @@ export default function Textform(props) {
     }
     const handlecleanclick=()=>
     {
-      setText();
+      setText("");
     }
+    const handlecopyclick=()=>
+      {
+        let text=document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+      }
+    // const handlespaceclick=()=>
+    //   {
+    //     let newText=text.split(/[ ]+/);
+    //     setText(newText.join[" "])
+    //   }
   const [text,setText]= useState();
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color:props.mode==="dark"?"white":"black"}}>
       <h1>{props.heading}</h1>
     <div className="mb-3">
-    <textarea className="form-control" value={text} onChange={handleonchange} id="myBox" rows="3"></textarea>
+    <textarea className="form-control" value={text} onChange={handleonchange} id="myBox" rows="3" style={{backgroundColor:props.mode==='dark'?'black':'white',color:props.mode==='dark'?'white':'black'  }}></textarea>
     </div>
     <button className="btn btn-primary mt-4 p-2" onClick={handleupclick}>Convert to uppercase</button>
     <button className="btn btn-primary mt-4 mx-2 p-2" onClick={handlelpclick}>Convert to Lowercase</button>
     <button className="btn btn-primary mt-4 mx-2 p-2" onClick={handlecleanclick}>Clear Text</button>
+    <button className="btn btn-primary mt-4 mx-2 p-2" onClick={handlecopyclick}>Copy text</button>
+    {/* <button className="btn btn-primary mt-4 mx-2 p-2" onClick={handlespaceclick}>Remove space</button> */}
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color:props.mode==="dark"?"white":"black"}}>
       <h1>Your text Summary</h1>
-      <p>{text.split(' ').length} word and {text.length} character</p> 
-      <p>{0.008 * text.split(' ').length} Minutes Read</p> 
+      {/* <p>{text.split(' ').length} word and {text.length} character</p> 
+      <p>{0.008 * text.split(' ').length} Minutes Read</p>  */}
       <h2>Preview</h2>
-      <p>{text} </p>
+      <p>{text.length>0?text:"Enter something in above textarea"} </p>
     </div>
     </>
   )
